95f45eb784098ffbf4e76051417cd76cdab89662
Gustav Johansson <gustajo@axis.com>
2023-03-24 09:42:15 +0100

    mappings.lua: Add changing buffers, add git next/prev hunk

--- mappings.lua
+++ mappings.lua
@@ -1,5 +1,9 @@
 return {
   n = {
+    H = { function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end, desc = "Previous buffer" },
+    L = { function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end, desc = "Next buffer" },
+    ["<leader>gj"] = { function() require("gitsigns").next_hunk() end, desc = "Next Git hunk" },
+    ["<leader>gk"] = { function() require("gitsigns").prev_hunk() end, desc = "Previous Git hunk" },
     ["<leader>lH"] = { "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header" },
     ["<leader>v"] = { "VimWiki", desc = "VimWiki" },
   },
