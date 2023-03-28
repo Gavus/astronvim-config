62b7329964a0a355a6444db0258aade8291301fc
Gustav Johansson <gustajo@axis.com>
2023-03-28 14:08:15 +0200

    Move autoformat_enabled to options instead init

--- init.lua
+++ init.lua
@@ -1,11 +1,4 @@
 return {
   -- Set colorscheme to use
   colorscheme = "gruvbox",
-  lsp = {
-    formatting = {
-      format_on_save = {
-        enabled = false, -- Disable format on save globally.
-      },
-    },
-  },
 }
--- options.lua
+++ options.lua
@@ -3,6 +3,9 @@ return {
     linebreak = true, -- break at space.
     list = true, -- show certain characters.
     listchars = "tab:▷▷", -- show tabs.
-    colorcolumn = "80", -- display column at 80
+    colorcolumn = "80", -- display column at 80.
+  },
+  g = {
+    autoformat_enabled = false, -- disable auto formatting at start.
   },
 }
