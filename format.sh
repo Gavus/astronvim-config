d0fdd645399d94928946be25f5ec7f2da837a952
Gustav Johansson <gustajo@axis.com>
2023-12-07 11:32:29 +0100

    Add pre-commit
    Add a test run with pre-commit. It automatically formats bash and lua
    files, and checks for trailing whitespaces and more. Checkout
    .pre-commit-config.yaml for details
    
    Install with pip: pip3 install -r ./pip-requirements.txt and then
    pre-commit install
    
    Update Astronvim to v3.39.0
    
    Remove format.sh: Replaced by pre-commit

--- format.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-
-docker run --rm --user "$(id -u):$(id -g)" -v "$(pwd)":/src:z -w /src fnichol/stylua sh -c "stylua ."
--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=2068,2046
 
 REMOVE=0
-ASTRONVIM_VERSION="v3.38.2"
+ASTRONVIM_VERSION="v3.39.0"
 NVIM_VERSION="v0.9.4"
 
 help() {
