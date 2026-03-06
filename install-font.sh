45ef4c236f721d1f06ef3dd58c31c16fce785ba7
Gustav Johansson <gavus@me.com>
2026-03-06 09:28:05 +0100

    install-font: Update to 3.4.0 and add multiple fonts

--- install-font.sh
+++ install-font.sh
@@ -1,15 +1,20 @@
 #!/bin/bash -e
 
-VERSION="v3.3.0"
+VERSION="v3.4.0"
 ZIP="SourceCodePro.zip"
-FONT="SauceCodeProNerdFontMono-Regular.ttf"
+FONTPREFIX="SauceCodeProNerdFontMono"
+FONTS=()
 DESTDIR="$HOME/.local/share/fonts"
+
+for boldness in "Regular" "Bold" "Medium" "SemiBold"; do
+    FONTS+=("$FONTPREFIX-$boldness.ttf")
+done
 
 if [ ! -f "$ZIP" ] ; then
     wget "https://github.com/ryanoasis/nerd-fonts/releases/download/$VERSION/$ZIP"
 fi
 
 mkdir -p "$DESTDIR"
-unzip -j "$ZIP" "$FONT" -o -d "$DESTDIR"
+unzip -j "$ZIP" "${FONTS[@]}" -d "$DESTDIR"
 
 echo "Done."
