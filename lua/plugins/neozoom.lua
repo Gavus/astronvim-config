9a3c572da264e5e7e69d6856cff247250daeb606
Gustav Johansson <gustajo@axis.com>
2026-03-09 17:02:07 +0100

    Remove plugin:neozoom
    Never used anyway
    
    Change-Id: I7e6b5a92975b2cbb2b19cea744aa69ed7c882ad8

--- lua/plugins/neozoom.lua
+++
@@ -1,8 +0,0 @@
----@type LazySpec
-return {
-  "nyngwang/neozoom.lua",
-  opts = {},
-  require("which-key").add {
-    { "<C-w>z", "<cmd>NeoZoomToggle<CR>", prefix = "<C-w>", desc = "Zoom", mode = "n" },
-  },
-}
