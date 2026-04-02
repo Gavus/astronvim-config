dcb85529d4ca65a083584719963b3055ef6da368
Gustav Johansson <gavus@me.com>
2026-04-02 09:35:01 +0200

    Revert "install-font: Update to 3.4.0 and add multiple fonts"
    This reverts commit 45ef4c236f721d1f06ef3dd58c31c16fce785ba7.

--- install-font.sh
+++ install-font.sh
@@ -1,20 +1,15 @@
 #!/bin/bash -e
 
-VERSION="v3.4.0"
+VERSION="v3.3.0"
 ZIP="SourceCodePro.zip"
-FONTPREFIX="SauceCodeProNerdFontMono"
-FONTS=()
+FONT="SauceCodeProNerdFontMono-Regular.ttf"
 DESTDIR="$HOME/.local/share/fonts"
-
-for boldness in "Regular" "Bold" "Medium" "SemiBold"; do
-    FONTS+=("$FONTPREFIX-$boldness.ttf")
-done
 
 if [ ! -f "$ZIP" ] ; then
     wget "https://github.com/ryanoasis/nerd-fonts/releases/download/$VERSION/$ZIP"
 fi
 
 mkdir -p "$DESTDIR"
-unzip -j "$ZIP" "${FONTS[@]}" -d "$DESTDIR"
+unzip -j "$ZIP" "$FONT" -o -d "$DESTDIR"
 
 echo "Done."
